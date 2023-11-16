82e7fb285a2f47a2bcf93d60e75266436ac352b6
Sebastian Perez <spezuluaga@gmail.com>
2023-11-16 12:52:23 -0500

    changed text to related svgs in footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,15 +1,27 @@
+import linkedinSVG from "../../../../public/svg/linkedin.svg";
+import githubSVG from "../../../../public/svg/github.svg";
+import gmailSVG from "../../../../public/svg/gmail.svg";
+
 export default function FooterComponent() {
   return (
     <footer className="flex w-auto justify-center items-center border-t-2 border-gray-200 mt-4">
-      <section>
-        <a href="" className="m-2">
-          LinkedIn
+      <section className="flex">
+        <a
+          href="https://www.linkedin.com/in/sebastian-perez-zuluaga/"
+          target="_blank"
+          className="m-4"
+        >
+          <img src={linkedinSVG.src} alt="LinkedIn Icon" />
         </a>
-        <a href="" className="m-2">
-          Gmail
+        <a
+          href="https://github.com/Sebastian-pz"
+          target="_blank"
+          className="m-4"
+        >
+          <img src={githubSVG.src} alt="GitHub Icon" />
         </a>
-        <a href="" className="m-2">
-          Phone
+        <a href="/" className="m-4">
+          <img src={gmailSVG.src} alt="Gmail icon" />
         </a>
       </section>
     </footer>
