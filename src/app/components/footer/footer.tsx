26de5a51d01c1ae0daebea86c2126aa7c7799b62
Sebastian Perez <spezuluaga@gmail.com>
2023-11-16 12:32:35 -0500

    basic footer added

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,17 @@
+export default function FooterComponent() {
+  return (
+    <footer className="flex w-auto justify-center items-center border-t-2 border-gray-200 mt-4">
+      <section>
+        <a href="" className="m-2">
+          LinkedIn
+        </a>
+        <a href="" className="m-2">
+          Gmail
+        </a>
+        <a href="" className="m-2">
+          Phone
+        </a>
+      </section>
+    </footer>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "./components/navbar/navbar";
+import FooterComponent from "./components/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           <Navbar />
         </header>
         {children}
+        <FooterComponent />
       </body>
     </html>
   );
