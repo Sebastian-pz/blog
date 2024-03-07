3d08cdbcec85bdd1e816f3791e45c09fe7dd9ef5
Sebastian Perez <spezuluaga@gmail.com>
2024-03-07 10:26:50 -0500

    remove unnecessary margin bottom from cards

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -15,9 +15,9 @@ export default function Card({
   responsibilities,
 }: CardProps) {
   return (
-    <article className="mb-5 w-full mx-auto ">
+    <article className="mb-8 w-full mx-auto ">
       <div className="shadow-[0px_100px_80px_rgba(0,_0,_0,_0.07),_0px_41.8px_33.42px_rgba(0,_0,_0,_0.05),_0px_22.3px_17.87px_rgba(0,_0,_0,_0.04),_0px_12.5px_10.02px_rgba(0,_0,_0,_0.04),_0px_6.7px_5.32px_rgba(0,_0,_0,_0.03),_0px_2.8px_2.21px_rgba(0,_0,_0,_0.02)] bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-        <div className="mb-8">
+        <div className="mb-2">
           <h3 className="text-gray-900 font-bold text-xl mb-2 hover:text-secondary-color">
             <Link href={link}>{title}</Link>
           </h3>
