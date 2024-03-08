95d4bacce0176d4c1495cc6ba131084b3a2e6873
Sebastian Perez <spezuluaga@gmail.com>
2024-03-08 11:59:06 -0500

    Fix size of project cards

--- src/app/components/ProjectCard/ProjectCard.tsx
+++ src/app/components/ProjectCard/ProjectCard.tsx
@@ -18,23 +18,22 @@ export default function ProjectCard({
     <article
       className="
       shadow-[0px_100px_80px_rgba(0,_0,_0,_0.07),_0px_41.8px_33.42px_rgba(0,_0,_0,_0.05),_0px_22.3px_17.87px_rgba(0,_0,_0,_0.04),_0px_12.5px_10.02px_rgba(0,_0,_0,_0.04),_0px_6.7px_5.32px_rgba(0,_0,_0,_0.03),_0px_2.8px_2.21px_rgba(0,_0,_0,_0.02)]
-    bg-white w-[200px] h-[300px] xl:w-[275px] xl:h-[320] m-3 p-2 md:p-4 lg:p-6 hover:scale-110 duration-1000"
+    bg-white w-4/5 h-[200px] xl:w-[275px] xl:h-[320] m-3 hover:scale-110 duration-1000"
     >
       <img
         src=""
         alt={`Imagen de ${title}`}
-        className="w-[50px] h-[50px] sm:h-[100px] sm:w-[100px] lg:w-[200px] m-auto border mt-6 mb-2"
+        className="h-1/2 w-full border border-red-600"
         loading="lazy"
       />
       {link ? (
-        <h2 className="font-medium">
+        <h2 className="px-2 md:px-4 mt-2 font-medium">
           <a href={link}>{title}</a>
         </h2>
       ) : (
-        <h2 className="font-medium">{title}</h2>
+        <h2 className="px-2 md:px-4 mt-2 font-medium">{title}</h2>
       )}
-
-      <p className="text-xs text-slate-600">{description}</p>
+      <p className="px-2 md:px-4 text-sm text-slate-600">{description}</p>
     </article>
   );
 }
