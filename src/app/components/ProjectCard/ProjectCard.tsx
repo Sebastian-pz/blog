93678c1ae1d858711c05f0d9aa2747bee1a34aaa
Sebastian Perez <spezuluaga@gmail.com>
2024-03-08 12:49:52 -0500

    Fix styles for intermedia screens

--- src/app/components/ProjectCard/ProjectCard.tsx
+++ src/app/components/ProjectCard/ProjectCard.tsx
@@ -14,7 +14,7 @@ export default function ProjectCard({ project }: propsComponent) {
     <article
       className="
       shadow-[0px_100px_80px_rgba(0,_0,_0,_0.07),_0px_41.8px_33.42px_rgba(0,_0,_0,_0.05),_0px_22.3px_17.87px_rgba(0,_0,_0,_0.04),_0px_12.5px_10.02px_rgba(0,_0,_0,_0.04),_0px_6.7px_5.32px_rgba(0,_0,_0,_0.03),_0px_2.8px_2.21px_rgba(0,_0,_0,_0.02)]
-    bg-white w-4/5 h-[200px] xl:w-[275px] xl:h-[320] m-3 hover:scale-110 duration-1000"
+    bg-white w-4/5 max-w-[300px] h-[200px] xl:w-[275px] xl:h-[320] m-3 hover:scale-110 duration-1000"
     >
       <img
         src={src}
