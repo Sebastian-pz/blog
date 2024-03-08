1b871aeb15076918207dbbdb43b4c09fc9326718
Sebastian Perez <spezuluaga@gmail.com>
2024-03-08 11:49:37 -0500

    Abstract project constants

---
+++ src/app/utils/ProjectsConstants.ts
@@ -0,0 +1,40 @@
+const PROJECTS = [
+  {
+    title: "Ducker",
+    description: "Trabajo como desarrollador web para un aplicativo e-commerce",
+    dates: "Enero 2023 - Julio 2023",
+    link: "/",
+  },
+  {
+    title: "RockStar Place",
+    description: "Trabajo como desarrollador web para un aplicativo e-commerce",
+    dates: "Enero 2023 - Julio 2023",
+    link: "/",
+  },
+  {
+    title: "Rock Paper Scissors game",
+    description: "Trabajo como desarrollador web para un aplicativo e-commerce",
+    dates: "Enero 2023 - Julio 2023",
+    link: "/",
+  },
+  {
+    title: "Mi blog",
+    description: "Trabajo como desarrollador web para un aplicativo e-commerce",
+    dates: "Enero 2023 - Julio 2023",
+    link: "/",
+  },
+  {
+    title: "Math Challenge",
+    description: "Trabajo como desarrollador web para un aplicativo e-commerce",
+    dates: "Enero 2023 - Julio 2023",
+    link: "/",
+  },
+  // {
+  //   title: "Tetris",
+  //   description: "Trabajo como desarrollador web para un aplicativo e-commerce",
+  //   dates: "Enero 2023 - Julio 2023",
+  //   link: "/",
+  // },
+];
+
+export default PROJECTS
