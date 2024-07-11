6420e34526676f9595fcc2624689b4c7cf6552ac
Sebastian Perez <spezuluaga@gmail.com>
2024-07-11 16:56:29 -0500

    feat(code): internacionalizace postNavigation

--- src/app/components/PostsNavigation/PostNavigation.tsx
+++ src/app/components/PostsNavigation/PostNavigation.tsx
@@ -1,21 +1,26 @@
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import PersonalizedLink from "./PersonalizedLink";
 
 export default function PostNavigation() {
+  const t = useTranslations("postNavigation");
   const activeLocale = useLocale();
 
   return (
     <>
-      <h2 className="text-center mt-6 text-lg font-bold">
-        ¿Quieres ver algo en específico?
-      </h2>
+      <h2 className="text-center mt-6 text-lg font-bold">{t("title")}</h2>
       <div className="h-6 mt-3 flex justify-center text-white">
         <PersonalizedLink
           href={`/${activeLocale}/experience`}
-          label="Experiencia"
+          label={t("experience")}
         />
-        <PersonalizedLink href={`/${activeLocale}/project`} label="Proyectos" />
-        <PersonalizedLink href={`/${activeLocale}/opinion`} label="Opiniones" />
+        <PersonalizedLink
+          href={`/${activeLocale}/project`}
+          label={t("opinion")}
+        />
+        <PersonalizedLink
+          href={`/${activeLocale}/opinion`}
+          label={t("projects")}
+        />
       </div>
     </>
   );
