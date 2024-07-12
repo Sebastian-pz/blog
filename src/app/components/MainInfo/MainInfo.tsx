f084c0639ea2bd0ba6d78095acc684cd2ddeebe2
Sebastian Perez <spezuluaga@gmail.com>
2024-07-12 17:53:19 -0500

    feat(code): internacionalizace mainInfo

--- src/app/components/MainInfo/MainInfo.tsx
+++ src/app/components/MainInfo/MainInfo.tsx
@@ -1,20 +1,15 @@
+import { useTranslations } from "next-intl";
+
 export default function MainInfo() {
+  const t = useTranslations("mainInfo");
+
   return (
     <section className="bg-primary-color-100 rounded-lg p-4">
       <h2 className="text-x text-primary-color-500 font-semibold">
-        Información sobre el blog
+        {t("title")}
       </h2>
-      <p className="text-sm mb-2  mt-3 leading-7">
-        En este blog me enfoco en compartir las últimas actualizaciones sobre
-        mis proyectos, consejos de programación e información relevante de la
-        industria. ¡Vamos a codificar, aprender y crecer juntos! 💡👨‍💻
-      </p>
-      <p className="text-sm  mt-3 leading-7">
-        Si tienes alguna sugerencia ¡No dudes en contactarme!
-      </p>
-      <p className="text-sm mt-3 text-primary-color-600">
-        El blog aún se encuentra en construcción...
-      </p>
+      <p className="text-sm mb-2  mt-3 leading-7">{t("description")}</p>
+      <p className="text-sm  mt-3 leading-7">{t("callToAction")}</p>
     </section>
   );
 }
