bdde7fcb8ee96b41de278bc1637213c2e79d2620
Sebastian Perez <spezuluaga@gmail.com>
2024-07-12 14:48:46 -0500

    feat(code): translate post bcaa1e968b6a43daaea2f4638025fac8

--- src/app/utils/locale/en/posts.ts
+++ src/app/utils/locale/en/posts.ts
@@ -36,4 +36,32 @@ export const posts: Array<postInterface> = [
     author,
     creationDate: "01/16/2024",
   },
+  {
+    author: author,
+    creationDate: "07/08/2024",
+    id: "bcaa1e968b6a43daaea2f4638025fac8",
+    image: tddImg,
+    mediaType: "image",
+    type: PostTypes.opinion,
+    title: "Software Development Using TDD",
+    description: [
+      "This is a short post sharing my personal thoughts on working with TDD, but before diving into that, let's first explain what TDD is:",
+      "|subtitle| What is TDD?",
+      "Test-Driven Development is a programming practice that involves writing tests (usually unit tests) first, then writing the code to make those tests pass, and finally refactoring the written code. (Herranz, J. 2023)",
+      "|image|https://images.spiceworks.com/wp-content/uploads/2022/09/29095630/How-TDD-Works.png",
+      "Image sourced from: SpiceWorks.com",
+
+      "|subtitle| My Opinion on Working with Test-Driven Development",
+      "Recently, I worked on a project using TDD methodology, and it gave me some insight into why this methodology is so polarizing within the community.",
+      "I've firsthand heard the opinion that 'writing tests is tedious because I know my code works when I write it.' And I understand that, but from my perspective, tests aren't just for yourself; they're meant to help your colleague (or yourself in the future) who might need to modify the code and ensure they aren't breaking anything.",
+      "The primary purpose of Test-Driven Development isn't just to help you write code (though it does), it wants to ensure that future changes don't affect the overall functionality of the application. It's a safeguard to prevent changes from breaking everything.",
+      "For example, let's say you worked on a feature to schedule users in a calendar, and it works perfectly. But tomorrow, you're asked to change the user schema. Both features work perfectly on their own, but when you integrate them, you might find it doesn't compile. Maybe the change in the user schema affected the scheduling process—perhaps a property, maybe a state, or anything else. TDD helps you foresee these issues.",
+      "Working with TDD means more development time, questioning whether 'the code works anyway,' and dealing with the pressure to deliver. But it helps develop more robust, scalable code and makes your teammates's jobs easier (or your own in the future) by avoiding bugs.",
+      "Yes, it's more code, but it's worth it.",
+      "|note| Note: It should be clarified that using TDD or not is a specific decision to each project. Some projects will benefit from it, while others may not. It's not a golden rule that must always be followed.",
+      "",
+
+      "Herranz, Jose Ignacio. (April 13, 2023). TDD as a Software Design Methodology, retrieved from: https://www.paradigmadigital.com/dev/tdd-como-metodologia-de-diseno-de-software/",
+    ],
+  },
 ];
