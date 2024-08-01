6d71d568b5e5ebca8f749529a8d722994e490abe
Sebastian Perez <spezuluaga@gmail.com>
2024-08-01 13:58:08 -0500

    feat(styles): add hover effects to Tags

--- src/app/components/Tags/Tag/Tag.tsx
+++ src/app/components/Tags/Tag/Tag.tsx
@@ -9,7 +9,7 @@ export default function Tag({ label, link }: propsComponent) {
   return (
     <Link
       href={link}
-      className="rounded-full bg-primary-color-100 border border-primary-color-300 px-4 py-1 text-sm font-medium text-muted-foreground hover:bg-accent hover:text-accent-foreground "
+      className="rounded-full bg-primary-color-100 border border-primary-color-300 px-4 py-1 text-sm font-medium text-muted-foreground hover:bg-accent hover:text-accent-foreground hover:bg-primary-color-200 duration-300"
       prefetch={false}
     >
       {label}
