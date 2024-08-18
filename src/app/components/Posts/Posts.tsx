bee2041dfd09e100947b3c1cc191fc614ebdfb19
Sebastian Perez <spezuluaga@gmail.com>
2024-08-17 22:07:17 -0500

    feat(code): add pagination

--- src/app/components/Posts/Posts.tsx
+++ src/app/components/Posts/Posts.tsx
@@ -1,11 +1,23 @@
 import { useLocale } from 'next-intl'
 
-import { getPosts } from '@/app/utils/const'
+import {
+  getFilteredPosts,
+  queryFilterI,
+  DEFAULT_POST_LIMIT,
+} from '@/app/utils/const'
 import Post from '@/components/Post/Post'
+import Pagination from '@/components/Pagination/Pagination'
 
 export default function Posts() {
   const localeActive = useLocale()
-  const posts = getPosts(localeActive)
+  const defaultPage = 1
+
+  const queryConfig: queryFilterI = {
+    page: defaultPage,
+  }
+  const { posts, totalPosts } = getFilteredPosts(localeActive, queryConfig)
+  const shouldRenderPagination = totalPosts > DEFAULT_POST_LIMIT
+
   return (
     <main>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
@@ -13,6 +25,13 @@ export default function Posts() {
           return <Post post={post} key={i} />
         })}
       </div>
+      {shouldRenderPagination && (
+        <Pagination
+          actualPage={defaultPage}
+          pages={Math.ceil(totalPosts / DEFAULT_POST_LIMIT)}
+          tag={null}
+        />
+      )}
     </main>
   )
 }
