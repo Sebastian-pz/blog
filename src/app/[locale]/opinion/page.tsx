bf796dd14cdc09c2690692bfc315440f30dcb486
Sebastian Perez <spezuluaga@gmail.com>
2024-10-04 12:45:10 -0500

    feat(metadata): add metadata into opinion page

--- src/app/[locale]/opinion/page.tsx
+++ src/app/[locale]/opinion/page.tsx
@@ -1,7 +1,31 @@
 import FilteredPosts from '@/components/FilteredPosts/FilteredPosts'
 import { getPostsByType } from '@/utilities/const'
 import { PostTypes } from '@/utilities/interfaces'
+import { Metadata } from 'next'
 import { useLocale, useTranslations } from 'next-intl'
+
+export const metadata: Metadata = {
+  title: 'Opinion posts',
+  description:
+    'Stay up to date with the latest trends in software development and technology. In Opinion Posts, Sebastian Perez shares his vision on code, new tools, and the future of development - in a language everyone can understand!',
+  keywords: [
+    'Sebastian Perez',
+    'Software developer',
+    'Programming',
+    'Skills',
+    'Trending',
+    'Opinion',
+    'Blog',
+    'Tech',
+    'TypeScript',
+    'JavaScript',
+    'NodeJS',
+    'React',
+    'NextJS',
+    'Python',
+    'Git',
+  ],
+}
 
 export default function Page() {
   const localeActive = useLocale()
