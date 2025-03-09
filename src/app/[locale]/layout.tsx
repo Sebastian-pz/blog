e8059027882d0d2b007a5581c7c0081930e14253
Sebastian Perez <spezuluaga@gmail.com>
2025-03-09 01:00:29 -0500

    refactor (layout): update the way that we are using fonts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,23 +2,18 @@ import './globals.css'
 
 import React from 'react'
 import type { Metadata } from 'next'
-import { Roboto as font } from 'next/font/google'
 import { NextIntlClientProvider } from 'next-intl'
 import { getLocale } from 'next-intl/server'
 
 import FooterComponent from '@/app/components/footer/footer'
 import Navbar from '@/app/components/NavBar/NavBarComponent'
+import { roboto } from '@/app/ui/fonts'
 
 export const metadata: Metadata = {
   title: 'Blog - Sebastian Perez Dev',
   description:
     'Technology blog, created by Sebastian Perez. Find articles, tips and resources about web development and software in general.',
 }
-
-const fontSettings = font({
-  weight: ['300', '400', '500'],
-  subsets: ['latin'],
-})
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -29,23 +24,13 @@ interface RootLayoutProps {
 
 export default async function RootLayout({
   children,
-  params,
 }: Readonly<RootLayoutProps>) {
-  // Use getLocale from next-intl/server to properly handle the locale in a server component
   const locale = await getLocale()
-  
-  // Load messages for the current locale
-  let messages;
-  try {
-    messages = (await import(`../../../messages/${locale}.json`)).default;
-  } catch (error) {
-    // Fallback to default locale if messages for the requested locale are not found
-    messages = (await import(`../../../messages/en.json`)).default;
-  }
+  const messages = (await import(`../../../messages/${locale}.json`)).default
 
   return (
     <html lang={locale}>
-      <body className={`${fontSettings.className} bg-bg-color`}>
+      <body className={`${roboto.className} bg-bg-color`}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <header className="mb-20">
             <Navbar />
