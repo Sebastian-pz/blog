1e7a5698f6d565db362f12eb70ce2ba3fa4baed7
Sebastian Perez <spezuluaga@gmail.com>
2025-03-10 11:54:18 -0500

    feat (social page): update social page and add i18n in remaining paragraph

---
+++ src/app/[locale]/social/utils/constants.ts
@@ -0,0 +1,17 @@
+import xSocialMediaSVG from '@/public/svg/x-social-media.svg'
+import GitHubSVG from '@/public/svg/github.svg'
+import LinkedInSVG from '@/public/svg/linkedin.svg'
+
+export const socialMediaProfiles = [
+  {
+    name: 'LinkedIn',
+    icon: LinkedInSVG,
+    url: 'https://www.linkedin.com/in/sebastian-perez-zuluaga/',
+  },
+  { name: 'GitHub', icon: GitHubSVG, url: 'https://github.com/sebastian-pz' },
+  {
+    name: 'X (Twitter)',
+    icon: xSocialMediaSVG,
+    url: 'https://twitter.com/sebastiantfa',
+  },
+]
